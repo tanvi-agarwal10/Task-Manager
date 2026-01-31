f3700e04787ab9ef098fd7b0ee91a029ae44d30f
Tanvi Agarwal <tanviagarwal@Tanvis-MacBook-Air.local>
2026-01-31 23:26:20 +0530

    chore: update server dependencies and configuration

--- server/server.js
+++ server/server.js
@@ -5,11 +5,8 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET","POST","PUT","DELETE"],
-  allowedHeaders: ["Content-Type"]
-}));
+aapp.use(cors());
+
 
 app.use(express.json());
 
