af194ac96618ffb8cce3d2230245ab9993a6954b
Tanvi_Agarwal <tanvi.ag10@gmail.com>
2026-02-01 15:53:45 +0530

    Change API base URL to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://task-manager-qwmq.onrender.com/api",
 });
 
 export default API;
